8a04628e9c3016499dede70d6f8c02540d174e2e
GiadaGallitto <gallitto.giada@gmail.com>
2023-01-24 16:48:36 +0100

    Change GET with POST and fix custom files

--- index.php
+++ index.php
@@ -4,22 +4,30 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Php Intro</title>
 </head>
 
 <body>
-    <header>
+    <header class="text-center">
         <h1>Parole proibite</h1>
+        <h4>Scrivi il tuo testo personale ed una parola che vuoi censurare al suo interno, una volta inviato, potrai vedere il tuo parafrago scritto, e al di sotto il paragrafo con le parole scelte censurate da degli asterischi</h4>
+        <h2>Buona scrittura!</h2>
     </header>
-    <main>
-        <form action="response.php" method="GET">
-            <label for="paragraph">Inserisci il tuo testo</label>
-            <input type="text" id="paragraph" name="paragraph">
+    <main class="text-center">
+        <form action="response.php" method="POST">
+            <div class="text mb-3 row">
+                <label for="paragraph">Inserisci il tuo testo</label>
+                <input type="text" class="w-25 mx-auto" id="paragraph" name="paragraph">
+            </div>
 
-            <label for="censured">Inserisci la tua parola da censurare</label>
-            <input type="text" id="censured" name="censured">
+            <div class="word mb-3 row">                
+                <label for="censured">Inserisci la tua parola da censurare</label>
+                <input type="text" class="w-25 mx-auto" id="censured" name="censured">
+            </div>
 
-            <button type="submit">Invia</button>
+            <button class="btn btn-success" type="submit">Invia</button>
         </form>
     </main>
 </body>
--- response.php
+++ response.php
@@ -1,21 +1,57 @@
-<p>
-    <?php
-        $normalParagraph = $_GET['paragraph'];
-        echo $normalParagraph;
-        echo strlen($normalParagraph);
-    ?>
-</p>
 
-<p>
-    <?php
-        $wordCensured = $_GET['censured'];
-    ?>
-</p>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <title>Document</title>
+    </head>
 
-<p>
-    <?php
-        $newParagraph = str_ireplace($wordCensured, '***', $normalParagraph);
-        echo $newParagraph;
-        echo strlen($newParagraph);
-    ?>
-</p>
+    <body>
+        <header class="text-center mb-5">
+            <h1>Ecco a te il tuo risultato!</h1>
+        </header>
+        
+        <main class="text-center">
+            <div class="mb-5">
+                <h4 class="text-danger">Paragrafo originale:</h4>
+                <p class="fs-5">
+                    <?php
+                        $normalParagraph = $_POST['paragraph'];
+                        echo $normalParagraph;
+                    ?>
+                    <span class="d-block fs-6 fw-bold">
+                        Lunghezza =
+                        <?php 
+                            echo strlen($normalParagraph);
+                        ?>
+                        parole
+                    </span>
+                </p>
+            </div>
+            
+            <div>
+                <h4 class="text-success">Paragrafo censurato:</h4>
+                <p class="fs-5">
+                    <?php
+                        $wordCensured = $_POST['censured'];
+                    ?>
+                    <?php
+                        $newParagraph = str_ireplace($wordCensured, '***', $normalParagraph);
+                        echo $newParagraph;
+                    ?>
+                    <span class="d-block fs-6 fw-bold">
+                    Lunghezza =
+                        <?php 
+                            echo strlen($newParagraph);
+                        ?>
+                        parole
+                    </span>
+                </p>
+            </div>
+        </main>
+    </body>
+</html>
