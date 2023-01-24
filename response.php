1f97eadb2499e8c3a9d1fcd611eba76a40494a77
GiadaGallitto <gallitto.giada@gmail.com>
2023-01-24 15:41:26 +0100

    Add replacement of censured word and lengths <p>

--- response.php
+++ response.php
@@ -1,3 +1,21 @@
-<?php
-    echo $_GET['paragraph']
-?>
+<p>
+    <?php
+        $normalParagraph = $_GET['paragraph'];
+        echo $normalParagraph;
+        echo strlen($normalParagraph);
+    ?>
+</p>
+
+<p>
+    <?php
+        $wordCensured = $_GET['censured'];
+    ?>
+</p>
+
+<p>
+    <?php
+        $newParagraph = str_ireplace($wordCensured, '***', $normalParagraph);
+        echo $newParagraph;
+        echo strlen($newParagraph);
+    ?>
+</p>
